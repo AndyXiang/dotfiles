ec3f6e42657c9bac70ffb0f76188cd03df8abc97
Junyi Xiang <xjy03@hust.edu.cn>
2025-05-20 00:13:48 +0800

    adding luasnip. Though snippets are not working.

---
+++ xvim/.config/nvim/LuaSnip/markdown/greek.lua
@@ -0,0 +1,35 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+
+return {
+  s({ trig = ";a", snippetType = "autosnippet" }, { t("\\alpha") }),
+  s({ trig = ";b", snippetType = "autosnippet" }, { t("\\beta") }),
+  s({ trig = ";g", snippetType = "autosnippet" }, { t("\\gamma") }),
+  s({ trig = ";r", snippetType = "autosnippet" }, { t("\\rho") }),
+  s({ trig = ";e", snippetType = "autosnippet" }, { t("\\epsilon") }),
+  s({ trig = ";t", snippetType = "autosnippet" }, { t("\\theta") }),
+  s({ trig = ";p", snippetType = "autosnippet" }, { t("\\phi") }),
+  s({ trig = ";o", snippetType = "autosnippet" }, { t("\\omega") }),
+  s({ trig = ";A", snippetType = "autosnippet" }, { t("\\Alpha") }),
+  s({ trig = ";B", snippetType = "autosnippet" }, { t("\\Beta") }),
+  s({ trig = ";G", snippetType = "autosnippet" }, { t("\\Gamma") }),
+  s({ trig = ";O", snippetType = "autosnippet" }, { t("\\Omega") }),
+  s({ trig = "psi", snippetType = "autosnippet" }, { t("\\psi") }),
+  s({ trig = "eta", snippetType = "autosnippet" }, { t("\\eta") }),
+  s({ trig = "tau", snippetType = "autosnippet" }, { t("\\tau") }),
+}
--- xvim/.config/nvim/init.lua
+++ xvim/.config/nvim/init.lua
@@ -1,2 +1,32 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.options")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require("config.autocmds")
+    require("config.keymaps")
+  end,
+})
+
+-- Luasnip Configurations
+vim.cmd([[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]])
+
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/LuaSnip/" })
+require("luasnip").config.set_config({ -- Setting LuaSnip config
+
+  -- Enable autotriggered snippets
+  enable_autosnippets = true,
+
+  -- Use Tab (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = "<Tab>",
+})
